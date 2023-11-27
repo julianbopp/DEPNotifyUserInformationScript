285169b0962d6ca6ab0c859d6bd16ce8cffabf88
julianbopp <julian.bopp@tutanota.com>
2023-11-27 12:55:04 +0100

    Update readMunki script

--- readMunki.sh
+++ readMunki.sh
@@ -1,15 +1,19 @@
-#!/bin/zsh
+#!/bin/bash
 
 
 MUNKI_INSTALL_LOG="/Library/Managed Installs/Logs/Install.log"
 MUNKI_MSU_LOG="/Library/Managed Installs/Logs/ManagedSoftwareUpdate.log"
 
-tail -n 0 -f $MUNKI_MSU_LOG | while read LOGLINE
+tail -n 0 -f "$MUNKI_MSU_LOG" | while read LOGLINE
 do
     if [[ "${LOGLINE}" == *"Installing"* ]]; then
-        echo $LOGLINE
+        echo "Status: ${LOGLINE:27}" >> /var/tmp/depnotify.log
     fi
-    if [[ "${LOGLINE}" == *"Ending managedsoftwareupdate run"* ]]; then
+    if [[ "${LOGLINE}" == *"End managed installer session"* ]]; then
+        echo "Command: ${LOGLINE:27}" >> /var/tmp/depnotify.log
         pkill -P $$ tail
+            echo "Command: DeterminateManual: 0" >> /var/tmp/depnotify.log
+            echo "Status: Setup Complete! " >> /var/tmp/depnotify.log  
+            echo "Command: ContinueButton: Get Started!" >> /var/tmp/depnotify.log
     fi
 done
