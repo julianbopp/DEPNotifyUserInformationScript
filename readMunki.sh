28e743d0ec03d43fd731af40e0e08ecd4c232f01
julianbopp <julian.bopp@tutanota.com>
2023-12-04 13:00:01 +0100

    Update scripts

--- readMunki.sh
+++ readMunki.sh
@@ -1,19 +1,33 @@
-#!/bin/bash
-
+#!/bin/zsh
 
 MUNKI_INSTALL_LOG="/Library/Managed Installs/Logs/Install.log"
-MUNKI_MSU_LOG="/Library/Managed Installs/Logs/ManagedSoftwareUpdate.log"
+DEP_NOTIFY_LOG="/var/tmp/depnotify.log"
 
-tail -n 0 -f "$MUNKI_MSU_LOG" | while read LOGLINE
+# Make sure directories and log file exist
+mkdir -p "/Library/Managed Installs/Logs"
+touch $MUNKI_MSU_LOG
+
+echo "Running managedsoftwareupdate"
+/usr/local/munki/managedsoftwareupdate &
+
+tail -n 1 -f "$MUNKI_MSU_LOG" | while read LOGLINE
 do
-    if [[ "${LOGLINE}" == *"Installing"* ]]; then
-        echo "Status: ${LOGLINE:27}" >> /var/tmp/depnotify.log
+	echo "HELLO" >> "Users/test/test.txt"
+    if ([[ "${LOGLINE}" == *"Installing"* ]] || [[ "${LOGLINE}" == *"Downloading"* ]]) && [[ "${LOGLINE}" != *" at "* ]] && [[ "${LOGLINE}" != *" from "* ]]; then
+        echo "Status: ${LOGLINE:27}" >> $DEP_NOTIFY_LOG
+    fi
+    if [[ "${LOGLINE}" == *"Ending managedsoftwareupdate run"* ]]; then
+    	echo "Ending managedsoftwareupdate run"
+    	echo "Running managedsoftwareupdate --installonly"
+    	/usr/local/munki/managedsoftwareupdate --installonly &
     fi
     if [[ "${LOGLINE}" == *"End managed installer session"* ]]; then
-        echo "Command: ${LOGLINE:27}" >> /var/tmp/depnotify.log
+    	echo "End managed installer session"
+        echo "Telling DEPNotify that software installs are done"
+        echo "Command: ${LOGLINE:27}" >> $DEP_NOTIFY_LOG
         pkill -P $$ tail
-            echo "Command: DeterminateManual: 0" >> /var/tmp/depnotify.log
-            echo "Status: Setup Complete! " >> /var/tmp/depnotify.log  
-            echo "Command: ContinueButton: Get Started!" >> /var/tmp/depnotify.log
+            echo "Command: DeterminateManual: 0" >> $DEP_NOTIFY_LOG
+            echo "Status: Setup Complete! " >> $DEP_NOTIFY_LOG  
+            echo "Command: ContinueButton: Get Started!" >> $DEP_NOTIFY_LOG
     fi
 done
